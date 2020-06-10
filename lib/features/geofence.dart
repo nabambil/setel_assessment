2774c98c5ff81148de1f66c41994e1553d4b247f
nabambil <nabambil689@gmail.com>
2020-06-11 00:49:54 +0800

    create new function related to internet and geofence

---
+++ lib/features/geofence.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter_geofence/geofence.dart';
+
+enum TypeFence {
+  inside,
+  outside
+}
+
+class GeoFenceModule {
+  GeoFenceModule(){
+    Geofence.initialize();
+  }
+
+  // to setup current location of devices as geofence
+  Future<void> setCurrentGeoAsFence({@required double radius}){
+    return Future.value();
+  }
+
+  // to setup manually by given info as geofence
+  Future<void> setGeoAsFence({@required double lat, @required double lng}){
+    return Future.value();
+  }
+
+  Future<bool> removeGeofence(){
+    return Future.value(true);
+  }
+
+  TypeFence statusOfFence(){
+    return TypeFence.outside;
+  }
+}
+
